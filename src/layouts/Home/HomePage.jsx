63b52a748f198e0cd4b824ea9a02287c6d0cd2b0
herin26it <phuonglinh2612004@gmail.com>
2025-12-23 13:18:02 +0700

    mfix-heart-2

--- src/layouts/Home/HomePage.jsx
+++ src/layouts/Home/HomePage.jsx
@@ -125,7 +125,9 @@ const HomePage = () => {
         <div className={`envelope-container ${isOpen ? 'is-open' : ''}`} onClick={handleEnvelopeClick}>
           <div className="envelope-flap"></div>
           <div className="envelope-base"></div>
-         <div className="wax-seal-img"><span>{"❤\uFE0E"}</span></div>
+         <div className="wax-seal-img"><span><svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+          </svg></span></div>
           <img src={WEDDING_DATA.assets.couplePhoto} alt="Couple" className="pop-up-photo" />
         </div>
 
